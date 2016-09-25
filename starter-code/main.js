5f5bed6af9cd3224aec70ab247a3b7d07d15ce7b
Zutaar <leon.falvey@gmail.com>
2016-09-25 20:46:41 +1000

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,7 +2,7 @@ var cardOne="queen";
 var cardTwo="queen";
 var cardThree="king";
 var cardFour="king";
-
+/*
 if (cardOne===cardTwo) {
 	alert("You found a match!");
 } else {
@@ -17,4 +17,15 @@ if (cardOne===cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
+}*/
+var gameBoard = document.getElementById("game-board");
+
+var createCards = function() {
+	var card = document.createElement('div');
+	card.className="card";
+	gameBoard.appendChild(card);
+
+}
+for (var i=0;i<10;i++) {
+	createCards();
 }
